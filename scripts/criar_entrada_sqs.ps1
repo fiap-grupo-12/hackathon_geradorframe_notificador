e31af8e3c94a8ac7d4fe381f6e9c8a167339c547
cesaraperes <cesaraperes@gmail.com>
2025-01-27 01:39:27 -0300

    Update criar_entrada_sqs.ps1

--- scripts/criar_entrada_sqs.ps1
+++ scripts/criar_entrada_sqs.ps1
@@ -1,1 +1,1 @@
-aws sqs send-message --queue-url https://sqs.us-east-1.amazonaws.com/339712873428/email-sqs-queue --message-body '{"Nome": "Cesar", "Email": "cesaraperes@gmail.com", "Assunto": "Imagens geradas com sucesso 10", "Corpo": "Olá, veja as imagens no link abaixo"}'
+aws sqs send-message --queue-url https://sqs.us-east-1.amazonaws.com/730335346157/email-sqs-queue --message-body '{"Nome": "Cesar", "Email": "cesaraperes@gmail.com", "Assunto": "Imagens geradas com sucesso 100", "Corpo": "Olá, veja as imagens no link abaixo"}'
